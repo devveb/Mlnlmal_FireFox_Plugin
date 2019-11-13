85655eb52f4fc44573065c009fd3f65184a2224f
taehyeong.kim <taehyeong.kim@yap.net>
2019-11-13 18:34:47 +0900

    figure out url

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
+function logTabs(tabs) {
+    let tab = tabs[0]; // Safe to assume there will only be one result
+    console.log(tab.url);
+    return tab.url;
+}
+
 
 function mis() {
-    console.log("url: "+window.location)
+    var url = browser.tabs.query({currentWindow: true, active: true}).then(logTabs, console.error);
+    console.log("mis url: "+url);
     var request = new XMLHttpRequest();
-    request.open("POST", "http://mlnlmal.ml/api/shlk?v=3&orl="+window.top.location.href);
+    request.open("POST", "http://mlnlmal.ml/api/shlk?v=3&orl="+url);
     request.onreadystatechange = function() {
         if(this.readyState === 4 && this.status === 200) {
             console.log(this.responseText);
