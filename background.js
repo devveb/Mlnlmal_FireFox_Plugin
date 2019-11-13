7e8672f1d33a5a62ec1ff2ba3cc8586e641119d1
devveb <dev.taek@gmail.com>
2019-11-14 01:05:10 +0900

    popup done

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 function logTabs(tabs) {
     let tab = tabs[0]; // Safe to assume there will only be one result
-    console.log(tab.url);
     return tab.url;
 }
 
@@ -8,25 +7,33 @@ function logTabs(tabs) {
 function mis() {
     var url = browser.tabs.query({currentWindow: true, active: true}).then(logTabs, console.error);
     console.log("mis url: "+url);
+
     var request = new XMLHttpRequest();
     request.open("POST", "http://mlnlmal.ml/api/shlk?v=3&orl="+url);
     request.onreadystatechange = function() {
+        console.log(this);
         if(this.readyState === 4 && this.status === 200) {
-            console.log(this.responseText);
-            var json = JSON.parse(this.responseText);
-            console.log(json);
-            var su = json.shortUrl;
-            var input = document.createElement('textarea');
-            document.body.appendChild(input);
-            input.value = su;
-            input.focus();
-            input.select();
-            document.execCommand('Copy');
-            input.remove();
-            console.log("'"+su+"' is copied to clipboard.");
+
+            //var json = JSON.parse(this.responseText);
+            //console.log(json);
         }
+
+
     };
+    navigator.clipboard.writeText("test11");
     request.send();
+
+    browser.notifications.create({
+        "type": "basic",
+        "iconUrl": browser.extension.getURL("icons/link-48.png"),
+        "title": "mlnlmal",
+        "message": "helloworld"
+    });
+
+
+
+
+
 }
 
 browser.browserAction.onClicked.addListener(mis);
